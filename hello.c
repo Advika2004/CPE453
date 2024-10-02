2bb1b78a3acbe104ab8a5af651ba9ff128c4c5b8
Advika Mahesh Deodhar <amdeodha@unix3.csc.calpoly.edu>
2024-10-01 21:48:24 -0700

    help me please restarting it all please help

--- hello.c
+++ hello.c
@@ -1,4 +1,4 @@
-#include <malloc.h>
+#include "malloc.h"
 
 int main() {
 // Step 1: Initialize the heap
@@ -9,7 +9,8 @@ int main() {
     }
 
     // Step 2: Request exactly the maximum amount of memory available
-    uint64_t max_memory = 65489;  // Maximum usable memory after the first header
+    // Maximum usable memory after the first header
+    uint64_t max_memory = 65489;  
     printf("\nRequesting exact maximum allocation: %zu bytes.\n", max_memory);
     void* ptr1 = my_malloc(max_memory);
     if (ptr1 != NULL) {
@@ -18,42 +19,13 @@ int main() {
         printf("Allocation failed.\n");
     }
 
-    // // Step 3: Request more than available memory (should trigger more heap allocation)
-    // uint64_t over_memory = 66000;  // More than available memory
-    // printf("\nRequesting more than available memory: %zu bytes.\n", over_memory);
-    // void* ptr2 = my_malloc(over_memory);
-    // if (ptr2 != NULL) {
-    //     printf("Allocated %zu bytes at %p\n", over_memory, ptr2);
-    // } else {
-    //     printf("Allocation failed.\n");
-    // }
-
-    // // Step 4: Request minimum allocation (16 bytes, because it's the smallest aligned size)
-    // uint64_t small_alloc = 1;  // Request 1 byte, but it should allocate 16 bytes
-    // printf("\nRequesting minimum allocation: %zu bytes (should allocate 16).\n", small_alloc);
-    // void* ptr3 = my_malloc(small_alloc);
-    // if (ptr3 != NULL) {
-    //     printf("Allocated %zu bytes at %p\n", make_16(small_alloc), ptr3);
-    // } else {
-    //     printf("Allocation failed.\n");
-    // }
-
-    // // Step 5: Request a really large amount of memory (more than 64KB)
-    // uint64_t large_alloc = 700000;  // Large memory request (700KB)
-    // printf("\nRequesting large allocation: %zu bytes.\n", large_alloc);
-    // void* ptr4 = my_malloc(large_alloc);
-    // if (ptr4 != NULL) {
-    //     printf("Allocated %zu bytes at %p\n", large_alloc, ptr4);
-    // } else {
-    //     printf("Allocation failed.\n");
-    // }
-
     // Print heap status after allocations
     printf("\nHeap status after allocations:\n");
     ChunkHeader* current_chunk = startOfHeap;
     while (current_chunk != NULL) {
         printf("Chunk start: %p, Size: %" PRIu64 " bytes, Is it free?: %d\n", 
-               (void*)current_chunk, current_chunk->size, current_chunk->is_it_free);
+               (void*)current_chunk, current_chunk->size, 
+               current_chunk->is_it_free);
         // Move to the next chunk
         current_chunk = current_chunk->next;
     }
--- malloc.h
+++ malloc.h
@@ -1,33 +1,50 @@
 #ifndef LIBMALLOC_H
 #define LIBMALLOC_H
-#include <stdio.h>     // For printf()
-#include <stdint.h>    // For uint64_t, uintptr_t
-#include <unistd.h>    // For sbrk()
-#include <errno.h>     // For errno and ENOMEM
-#include <inttypes.h>  // For PRIu64
+#include <stdio.h>     
+#include <stdint.h>    
+#include <unistd.h>    
+#include <errno.h>     
+#include <inttypes.h>  
 
+ // For printf()
+ // For uint64_t, uintptr_t
+ // For sbrk()
+ // For errno and ENOMEM
+ 
 
-#define STANDARD_HEAP_SIZE 65536  // Define this as 64KB (you can change it if needed)
-#define CHUNK_HEADER sizeof(ChunkHeader)  // Size of the chunk header
+// Define this as 64KB (you can change it if needed)
+// Size of the chunk header
+#define STANDARD_HEAP_SIZE 65536  
+#define CHUNK_HEADER sizeof(ChunkHeader)  
 
-// Global variable to keep track of the very first start of the heap
-ChunkHeader *startOfHeap = NULL;
+// Global variable to keep track of the very first                     
+//start of the heap
+// ChunkHeader *startOfHeap = NULL;
 
 // Struct for the ChunkHeader
-#pragma pack(push, 1) // Turn off padding for the struct
+// Turn off padding for the struct
+// Size of the chunk
+// Flag indicating if the chunk is free
+// Pointer to the previous chunk in the linked list
+// Pointer to the next chunk in the linked list
+#pragma pack(push, 1) 
 typedef struct ChunkHeader {
-    uint64_t size;          // Size of the chunk
-    int is_it_free;         // Flag indicating if the chunk is free
-    struct ChunkHeader *prev;  // Pointer to the previous chunk in the linked list
-    struct ChunkHeader *next;  // Pointer to the next chunk in the linked list
+    size_t size;          
+    int is_it_free;         
+    struct ChunkHeader *prev;  
+    struct ChunkHeader *next;  
 } ChunkHeader;
-#pragma pack(pop) // Turn padding back on
+#pragma pack(pop) 
+// Turn padding back on
 
 // Struct for returning information about a free chunk
+// Pointer to the start of the free chunk
+// Size of the memory requested
+//Flag indicating if there is enough space//
 typedef struct ChunkInfo {
-    ChunkHeader *start_of_free_chunk_ptr;  // Pointer to the start of the free chunk
-    uint64_t amount_asked_for;             // Size of the memory requested
-    int enough_space;                      // Flag indicating if there is enough space
+    ChunkHeader *start_of_free_chunk_ptr;  
+    uint64_t amount_asked_for;             
+    int enough_space;                      
 } ChunkInfo;
 
 #endif
